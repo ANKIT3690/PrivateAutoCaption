e86223a4e07fe156e53ce898bafef06d94cc7ba4
ANKIT3690 <93529237+ANKIT3690@users.noreply.github.com>
2022-01-14 18:09:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "")
     CAPTION = os.environ.get("CAPTION", "@thewarriorsreal @defenderofthemultiverse")
     BUTTON_TEXT = os.environ.get("BUTTON", "🔻Join Channel🔻")
-    URL_LINK = os.environ.get("LINK", "T.ME/MO_TECH_YT")
+    URL_LINK = os.environ.get("LINK", "thewarriorsreal")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
